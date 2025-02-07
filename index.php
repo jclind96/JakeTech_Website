6d7c850a0f0d834fc7bbf66179a8358f13228397
jclind96 <148372564+jclind96@users.noreply.github.com>
2025-02-06 23:55:06 -0500

    Update index.php
    Added headers

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Awesome Website</title>
+</head>
+    
 <body>
     <header>
         <h1>Welcome to Your Website</h1>
